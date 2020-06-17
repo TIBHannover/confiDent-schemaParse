e5d8fb2354cfda46f8feda0dd3f70fd18068c13f
Castro0o <andre@andrecastro>
2020-06-17 10:21:17 +0200

    clarifying property var name

--- schemaParse/DataCite2MW.py
+++ schemaParse/DataCite2MW.py
@@ -56,21 +56,23 @@ def dataciteSchema2dict(xmlcode: str, xs_uri: str) -> Dict:
     }
 
     # properties
-    for el in resource.findall('./xs:complexType/xs:all/xs:element',
+    for prop in resource.findall('./xs:complexType/xs:all/xs:element',
                                namespaces={'xs': xs_uri}):
         for tag in resource.findall('./'):  # direct child elements
             if 'Type' in tag.tag:
-                _type = tag.tag
-                _type = _type.replace(f'{{{XMLSchema}}}', '')  # rm schema uri
+                prop_type = tag.tag
+                prop_type = prop_type.replace(f'{{{XMLSchema}}}', '')  # rm schema uri
+
+
 
         documentation = ''
-        for doc in el.findall('.//xs:annotation/xs:documentation',
+        for doc in prop.findall('.//xs:annotation/xs:documentation',
                               namespaces={'xs': xs_uri}):
             documentation += doc.text
         documentation = documentation.replace('\n\n', '') # remove empty lines
-        datacite_els_dict[el.get('name')] = {
-            'name': el.get('name'),
-            'type': _type,
+        datacite_els_dict[prop.get('name')] = {
+            'name': prop.get('name'),
+            'type': prop_type,
             'kind': 'Property',
             'cardinality': 1, # TODO: Philip logic
             'definition': documentation,
