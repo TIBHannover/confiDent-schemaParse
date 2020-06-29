5a6ea6a194e5f60e874b54c40d60cb1f9519a780
Castro0o <andre@andrecastro>
2020-06-29 17:33:26 +0200

    basic test: is there output?

--- schemaparse/tests/tests.py
+++ schemaparse/tests/tests.py
@@ -1,9 +1,19 @@
 # import pytest
+# import sys, os
+# testpath = os.path.dirname(os.path.abspath(__file__))
+# sys.path.insert(0, testpath + '/../')
+from schemaparse import app
 
 
-def test_sum():
-    assert sum([1, 2, 3]) == 6, "Should be 6"
+def test_output_mw():
+    schema_wiki = app.schema2mw(_schema='DataCite')
+    assert len(schema_wiki) > 0
 
+# print(schema_wiki)
 
-def test_sum_tuple():
-    assert sum((1, 2, 2)) == 5, "Should be 6"
+# def test_sum():
+#     assert sum([1, 2, 3]) == 6, "Should be 6"
+#
+#
+# def test_sum_tuple():
+#     assert sum((1, 2, 2)) == 5, "Should be 6"
